9c20749ff673764867215909f7f7d028f27b2b20
Steve Finch <springminutes@gmail.com>
2011-12-03 06:20:41 -0500

    Incorporated feedback from Guillaume Laforge

--- src/main/java/org/springminutes/example/integration/JsonToFloodRequestXmlTransformer.groovy
+++ src/main/java/org/springminutes/example/integration/JsonToFloodRequestXmlTransformer.groovy
@@ -13,22 +13,22 @@ class JsonToFloodRequestXmlTransformer extends AbstractPayloadTransformer<String
         def xml = new MarkupBuilder(writer);
 
         xml.REQUEST_GROUP(MISMOVersionID: "2.4") {
-            SUBMITTING_PARTY(LoginAccountIdentifier: "${json.credentials.username}",
-                    LoginAccountPassword: "${json.credentials.password}")
-            REQUEST(InternalAccountIdentifier: "${json.credentials.account}",
-                    RequestDatetime: "${DateTime.now()}") {
+            SUBMITTING_PARTY(LoginAccountIdentifier: json.credentials.username,
+                    LoginAccountPassword: json.credentials.password)
+            REQUEST(InternalAccountIdentifier: json.credentials.account,
+                    RequestDatetime: DateTime.now().toDateTimeISO()) {
                 REQUEST_DATA() {
                     FLOOD_REQUEST(MISMOVersionID: "2.4", _ActionType: "Original") {
                         _PRODUCT(_CategoryDescription: "Flood") {
                             _NAME(_Identifier: "FL")
                         }
-                        BORROWER(_FirstName: "${json.loanInfo.borrower.firstName}",
-                                _LastName: "${json.loanInfo.borrower.lastName}")
-                        MORTGAGE_TERMS(LenderCaseIdentifier: "${json.loanInfo.caseNumber}")
-                        PROPERTY(_StreetAddress: "${json.loanInfo.property.address1}",
-                                _City: "${json.loanInfo.property.city}",
-                                _State: "${json.loanInfo.property.state}",
-                                _PostalCode: "${json.loanInfo.property.zip}") {}
+                        BORROWER(_FirstName: json.loanInfo.borrower.firstName,
+                                _LastName: json.loanInfo.borrower.lastName)
+                        MORTGAGE_TERMS(LenderCaseIdentifier: json.loanInfo.caseNumber)
+                        PROPERTY(_StreetAddress: json.loanInfo.property.address1,
+                                _City: json.loanInfo.property.city,
+                                _State: json.loanInfo.property.state,
+                                _PostalCode: json.loanInfo.property.zip) {}
                     }
                 }
             }
